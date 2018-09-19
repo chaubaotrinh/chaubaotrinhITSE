a4d8dc437715a011c0704fb4566a7fc0926bc2f5
chau.trinh <chau.trinh@my.tccd.edu>
2018-09-19 18:48:32 -0500

    Section 2 continue

---
+++ Classwork/Section2/ITSE1430.MovieLib.UI/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITSE1430.MovieLib.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            //call close function 
+            if (MessageBox.Show("Are you sure you want to exit?", "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            // "Close" - caption of the dialog => the second string is the title of the message box
+            Close();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )  //change aboutToolStripMenuItem_Click to OnHelpAbout
+        {
+            //aboutToolStripMenuItem. default variable name, then change to _miHelpAbout in Name property
+            MessageBox.Show(this, "Sorry", "Help", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //MessageBox.Show method use the predifined button, change the caption and add the message only!
+            // add help icon: add "this," in the beginning and icon code should be in the last position 
+        }
+    }
+}
