6a59694bff95a3eed5cfca910cf8b1d77e710075
Chau Bao Trinh <chau.trinh@my.tccd.edu>
2018-11-04 12:27:25 -0600

    Update

--- Labs/ContactManager.UI/ContactManager/IMessageService.cs
+++ Labs/ContactManager.UI/ContactManager/IMessageService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* 
+ * Student: Chau Trinh
+ * Class: ITSE 1430
+ * Lab 3: Contact Manager
+ * Date: 5 Nov 2018
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
